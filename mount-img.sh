a8c7519e2733292049e1bd447095e0c1159a80d2
Dr-Willy <Dr-Willy@users.noreply.github.com>
2020-07-21 22:14:19 +1200

    Add first version of mount-img.sh

---
+++ mount-img.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Mount partitions from a disk image
+#
+
+img=$1
+mountPrefix=${2:-"/mnt"}
+
+partition="none"
+
+readarray partitions < <(partx -rg $img)
+
+for line in "${partitions[@]}";do
+    read i beg end sector size label < <(echo $line)
+    name=${label:-"${img%\.img}_$i"}
+    offset=$(($beg*512))
+    sizeLimit=$(($sector*512))
+    mountPoint=${mountPrefix}/${name}
+    echo "mounting $name on $mountPoint"
+
+    if [ ! -d $mountPoint ]; then
+        mkdir $mountPoint
+    fi
+
+    mount $img $mountPoint -o loop,offset=${offset},sizelimit=${sizeLimit}
+
+done
+
