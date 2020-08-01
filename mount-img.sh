2fb2cf8ca472d01993f7bff885ba8a0fc53a096c
Dr-Willy <Dr-Willy@users.noreply.github.com>
2020-08-02 00:20:45 +1200

    Add cli options and usage message

--- mount-img.sh
+++ mount-img.sh
@@ -5,6 +5,57 @@
 # Requirements: partx, losetup
 #
 
+check_requirements () {
+    if [ $(id -u) != 0 ]; then
+        echo "Please run this script as root"
+        exit 1
+    fi
+
+    requirements="partx losetup getopt"
+    for req in $requirements; do
+        if ! which $req>/dev/null; then
+            echo "Can not find $req"
+            exit 1
+        fi
+    done
+}
+
+usage () {
+    echo "$0 [-h] [--ro] [-p <part_id>] <image_file> <mount_point>"
+    exit 1
+}
+
+check_requirements
+
+if [[ $# -lt 2 ]]; then
+    usage
+    exit 1
+fi
+
+args=$(getopt -o "hp:" --long "ro" -n $0 -- $@)
+eval set -- "$args"
+
+while true; do
+    case "$1" in
+        -h)
+            usage
+            exit 0;;
+        -p)
+            partId=$2
+            shift 2;;
+        --ro)
+            mountOption="$mountOption,ro"
+            shift;;
+        --)
+            shift
+            break;;
+        *)
+            echo "unrecgnise argument: $1"
+            exit 1;;
+    esac
+done
+
+
 img=$1
 mountPrefix=${2:-"/mnt"}
 
@@ -12,19 +63,29 @@ partition="none"
 
 readarray partitions < <(partx -rg $img)
 
-for line in "${partitions[@]}";do
-    read i beg end sector size label < <(echo $line)
+partToMount="$(seq ${#partitions[@]})"
+if [ -v partId ];then 
+    if [[ $partId -le ${#partitions[@]} ]];then
+        partToMount="$partId"
+    else
+        echo "Partition ID out of range: ${#partitions[@]} partition(s) found"
+        exit 1
+    fi
+fi
+
+for i in $partToMount; do
+    read i beg end sector size label < <(echo ${partitions[$((i-1))]})
     name=${label:-"${img%\.img}_$i"}
     offset=$(($beg*512))
     sizeLimit=$(($sector*512))
     mountPoint=${mountPrefix}/${name}
-    echo "mounting $name on $mountPoint"
 
     if [ ! -d $mountPoint ]; then
         mkdir $mountPoint
     fi
 
-    mount $img $mountPoint -o loop,offset=${offset},sizelimit=${sizeLimit}
+    #mount $img $mountPoint -o loop,offset=${offset},sizelimit=${sizeLimit}
+    mount $img $mountPoint -o loop,offset=${offset},sizelimit=${sizeLimit}${mountOption}
 
 done
 
